af1ad61ba96a8aea8fbc8f24a2452806bd528372
Uriel Cayetano <al221510804@gmail.com>
2022-11-16 18:21:53 -0600

    Token

---
+++ app/Http/Controllers/AutenticarController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistroRequest;
+use App\Http\Requests\accesoRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+
+class AutenticarController extends Controller
+{
+    public function registro (RegistroRequest $request)
+    {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+
+            return response()->json([
+                'res' => true,
+                'msj' =>'Usuario Registrado correctamnete'
+            ],200);
+
+
+    }
+
+    public function acceso (accesoRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'msj' => ['Las credenciales son incorrectas!!!'],
+            ]);
+        }
+     
+            $token = $user->createToken($request->email)->plainTextToken;
+
+            return response()->json([
+                'res' => true,
+                'token' => $token
+            ]);
+        //return $user->createToken($request->device_name)->plainTextToken;
+    }
+}
